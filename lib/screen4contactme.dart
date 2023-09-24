968c0576dd2281e7c04a085ca53f239fa66ab547
Maha-ELMaghawry <144539301+Maha-ELMaghawry@users.noreply.github.com>
2023-09-24 21:10:25 +0300

    Updates

--- lib/screen4contactme.dart
+++ lib/screen4contactme.dart
@@ -12,54 +12,60 @@ class ContactMeScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.teal.shade200,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/ContactMe.jpeg'),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 30),
-            child: Text(
-              'Contact Me at WhatsApp :',
-              style: TextStyle(
-                color: Colors.brown,
-                fontSize: 20,
-                shadows: [
-                  Shadow(
-                    blurRadius: 5,
-                    color: Colors.tealAccent,
-                    offset: Offset(2, 2),
-                  )
-                ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 150,
+              height: 150,
+              child: Image.asset('assets/images/ContactMe.jpeg'),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 30),
+              child: Text(
+                'Contact Me at WhatsApp :',
+                style: TextStyle(
+                  color: Colors.brown,
+                  fontSize: 20,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 5,
+                      color: Colors.tealAccent,
+                      offset: Offset(2, 2),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          FlutterSocialButton(
-            iconColor: Colors.white,
-            buttonType: ButtonType.whatsapp,
-            mini: true,
-            onTap: () {
-              openWhatAppChat();
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: ElevatedButton(
-              onPressed: () => context.go('/calm'),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal.shade200),
-              child: const Text('Back'),
+            FlutterSocialButton(
+              iconColor: Colors.white,
+              buttonType: ButtonType.whatsapp,
+              mini: true,
+              onTap: () {
+                openWhatAppChat();
+              },
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: ElevatedButton(
-              onPressed: () => context.go('/welcome'),
-              style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal.shade200),
-              child: const Text('Back to Home-Screen'),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ElevatedButton(
+                onPressed: () => context.go('/calm'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal.shade200),
+                child: const Text('Back'),
+              ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ElevatedButton(
+                onPressed: () => context.go('/welcome'),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal.shade200),
+                child: const Text('Back to Home-Screen'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
